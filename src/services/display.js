95dbbb511ff65579514530360c17a70f5c2fff81
Insurmountable-dev <264776215+Insurmountable-dev@users.noreply.github.com>
2026-07-24 16:30:38 +0300

    Add service to display all farmer deliveries

--- src/services/display.js
+++ src/services/display.js
@@ -1,10 +1,31 @@
 const readFile = require("../utils/readFile.js");
+const sleep    = require("../utils/Sleep.js");
 
-function displayFarmers (callback) {
+async function displayFarmers(callback) {
 
-    console.log("All Farmers");
+    // Reading The Data 
+    const farmerNumbers = await readFile("farmerNumbers");
+    const farmerNames = await readFile("farmerNames");
+    const produceTypes = await readFile("produceTypes");
+    const quantities = await readFile("quantities");
+    const prices = await readFile("prices");
+    const paymentStatuses = await readFile("paymentStatus");
+
+    for (let i = 0; i < farmerNumbers.length; i++) {
+
+        console.log("==========================================");
+        console.log(`Farmer Number     : ${farmerNumbers[i]}`);
+        console.log(`Farmer Name       : ${farmerNames[i]}`);
+        console.log(`Produce Type      : ${produceTypes[i]}`);
+        console.log(`Quantity Delivered: ${quantities[i]}`);
+        console.log(`Price Per Unit    : KSh ${prices[i]}`);
+        console.log(`Payment Status    : ${paymentStatuses[i]}`);
+        console.log("==========================================");
+
+        await sleep(900);
+    }
+
     callback();
-    return;
 
 }
 
